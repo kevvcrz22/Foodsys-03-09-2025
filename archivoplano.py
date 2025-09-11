14164f04333cc0bb01c9a230346a3981f9a4dfc1
Diego santofimio <diegosantofimio328@gmail.com>
2025-09-11 14:44:40 -0500

    modifique unos archivos y agregue unos link

--- archivoplano.py
+++
@@ -1,6 +0,0 @@
-print("******************************************")
-print("programa que abre, lee, inprime archivo txt")
-print("******************************************")
-archivo = open("c:\\python\\Archivos_planos\\chavo.txt")
-
-print(archivo.read())
