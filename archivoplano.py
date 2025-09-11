412beaaf1df65f8342189e26c550b62b40adc646
Diego santofimio <diegosantofimio328@gmail.com>
2025-09-11 13:06:03 -0500

    guardar cambios

---
+++ archivoplano.py
@@ -0,0 +1,6 @@
+print("******************************************")
+print("programa que abre, lee, inprime archivo txt")
+print("******************************************")
+archivo = open("c:\\python\\Archivos_planos\\chavo.txt")
+
+print(archivo.read())
